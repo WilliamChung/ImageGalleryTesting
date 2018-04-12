53e3dd406e716bc8d1b830f9856dda2ddd6c79d0
William <williamc825@hotmail.com>
2018-04-12 15:31:41 -0700

    fourteenth commit, video part 14 finished.   Profile Settings     Load Profile Picture/Handle Default Picture     Add Universal Image Loader Library     PROBLEM: Cannot load web image with ImageLoader

--- app/src/main/java/com/mindandmatters/william/imagegallerytesting/Profile/EditProfileFragment.java
+++ app/src/main/java/com/mindandmatters/william/imagegallerytesting/Profile/EditProfileFragment.java
@@ -2,11 +2,15 @@ package com.mindandmatters.william.imagegallerytesting.Profile;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.mindandmatters.william.imagegallerytesting.R;
+import com.mindandmatters.william.imagegallerytesting.Utils.UniversalImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 /**
  * Created by lappy on 2018-04-11.
@@ -15,12 +19,29 @@ import com.mindandmatters.william.imagegallerytesting.R;
 public class EditProfileFragment extends android.support.v4.app.Fragment {
 
     private static final String TAG = "EditProfileFragment";
+    private ImageView mProfilePhoto;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_edit_profile, container, false);
 
+        mProfilePhoto = (ImageView) view.findViewById(R.id.profile_photo);
+
+        initImageLoader();
+        setProfileImage();
+
         return view;
+    }
+
+    private void initImageLoader(){
+        UniversalImageLoader universalImageLoader = new UniversalImageLoader(getActivity());
+        ImageLoader.getInstance().init(universalImageLoader.getConfig());
+    }
+
+    private void setProfileImage(){
+        Log.d(TAG, "setProfileImage: setting profile image.");
+        String imgURL = "www.androidcentral.com/sites/androidcentral.com/files/styles/xlarge/public/article_images/2016/08/ac-lloyd.jpg?itok=bb72IeLf";
+        UniversalImageLoader.setImage(imgURL, mProfilePhoto, null, "https://");
     }
 }
