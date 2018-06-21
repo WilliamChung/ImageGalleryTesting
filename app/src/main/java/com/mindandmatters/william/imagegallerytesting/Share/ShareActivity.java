7e061ee00a0edfa821d2c0e331ddbc608f4a03bc
William <williamc825@hotmail.com>
2018-06-20 17:15:37 -0700

    forty-first commit, video part 41 finished.   Share Activity     Added permissions to manifest.     New Util: Permissions     Implemented check permissions methods in ShareActivity.

--- app/src/main/java/com/mindandmatters/william/imagegallerytesting/Share/ShareActivity.java
+++ app/src/main/java/com/mindandmatters/william/imagegallerytesting/Share/ShareActivity.java
@@ -1,8 +1,10 @@
 package com.mindandmatters.william.imagegallerytesting.Share;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -11,6 +13,7 @@ import android.view.MenuItem;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.mindandmatters.william.imagegallerytesting.R;
 import com.mindandmatters.william.imagegallerytesting.Utils.BottomNavigationViewHelper;
+import com.mindandmatters.william.imagegallerytesting.Utils.Permissions;
 
 /**
  * Created by lappy on 2018-03-21.
@@ -20,6 +23,7 @@ public class ShareActivity extends AppCompatActivity {
     private static final String TAG = "ShareActivity";
     private Context mContext = ShareActivity.this;
     private static final int ACTIVITY_NUM = 2;
+    public static final int VERIFY_PERMISSIONS_REQUEST = 1;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -27,10 +31,56 @@ public class ShareActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: started.");
 
-        setupBottomNavigationView();
+        if(checkPermissionsArray(Permissions.PERMISSIONS)){
+        }
+        else{
+            verifyPermissions(Permissions.PERMISSIONS);
+        }
+
+        //setupBottomNavigationView();
 
     }
 
+    public void verifyPermissions(String[] permissions){
+        Log.d(TAG, "verifyPermissions: verifying permissions");
+
+        ActivityCompat.requestPermissions(
+                ShareActivity.this,
+                permissions,
+                VERIFY_PERMISSIONS_REQUEST
+        );
+    }
+
+    //check an array of permissions
+    public boolean checkPermissionsArray(String[] permissions){
+        Log.d(TAG, "checkPermissionsArray: setting up permissions array");
+
+        for (int i = 0; i < permissions.length; i++){
+
+            String check = permissions[i];
+            if(!checkPermissions(check)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    //check if a single permission has been granted
+    public boolean checkPermissions(String permission){
+        Log.d(TAG, "checkPermissions: checking permission" + permission);
+
+        int permissionRequest = ActivityCompat.checkSelfPermission(ShareActivity.this, permission);
+
+        if(permissionRequest != PackageManager.PERMISSION_GRANTED){
+            Log.d(TAG, "checkPermissions: \n Permission was not granted for: " + permission);
+            return false;
+        }
+        else{
+            Log.d(TAG, "checkPermissions: \n Permission was granted for: " + permission);
+            return true;
+        }
+    }
     private void setupBottomNavigationView(){
         Log.d(TAG, "setupBottomNavigationView: setting up bottom navigation view");
         BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
---
+++ app/src/main/java/com/mindandmatters/william/imagegallerytesting/Utils/Permissions.java
@@ -0,0 +1,15 @@
+package com.mindandmatters.william.imagegallerytesting.Utils;
+
+import android.Manifest;
+/**
+ * Created by lappy on 2018-06-20.
+ */
+
+public class Permissions {
+
+    public static final String[] PERMISSIONS = {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+    };
+}
