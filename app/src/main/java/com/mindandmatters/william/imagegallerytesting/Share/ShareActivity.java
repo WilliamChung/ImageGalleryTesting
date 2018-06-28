97ccd8194f5447cf649315ca62a64977bec0d2de
William <williamc825@hotmail.com>
2018-06-28 14:53:08 -0700

    forty-second commit, video part 42 finished.   Share Activity     added activity_share and layout_bottom_tabs     activity now opens proper page     bottom bar in activity can now be seen

--- app/src/main/java/com/mindandmatters/william/imagegallerytesting/Share/ShareActivity.java
+++ app/src/main/java/com/mindandmatters/william/imagegallerytesting/Share/ShareActivity.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -14,6 +16,7 @@ import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.mindandmatters.william.imagegallerytesting.R;
 import com.mindandmatters.william.imagegallerytesting.Utils.BottomNavigationViewHelper;
 import com.mindandmatters.william.imagegallerytesting.Utils.Permissions;
+import com.mindandmatters.william.imagegallerytesting.Utils.SectionsPagerAdapter;
 
 /**
  * Created by lappy on 2018-03-21.
@@ -25,10 +28,12 @@ public class ShareActivity extends AppCompatActivity {
     private static final int ACTIVITY_NUM = 2;
     public static final int VERIFY_PERMISSIONS_REQUEST = 1;
 
+    private ViewPager mViewPager;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_share);
         Log.d(TAG, "onCreate: started.");
 
         if(checkPermissionsArray(Permissions.PERMISSIONS)){
@@ -41,6 +46,22 @@ public class ShareActivity extends AppCompatActivity {
 
     }
 
+    private void setupViewPager() {
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        //adapter.addFragment(new GalleryFragment);
+        //adapter.addFragment(new PhotoFragment);
+
+//        mViewPager = (ViewPager) findViewById(R.id.container);
+//        mViewPager.setAdapter(adapter);
+//
+//        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabsBottom);
+//        tabLayout.setupWithViewPager(mViewPager);
+//
+//        tabLayout.getTabAt(0).setText(R.string.gallery);
+//        tabLayout.getTabAt(1).setText(R.string.photo);
+    }
+
+    //verify all permissions passed
     public void verifyPermissions(String[] permissions){
         Log.d(TAG, "verifyPermissions: verifying permissions");
 
