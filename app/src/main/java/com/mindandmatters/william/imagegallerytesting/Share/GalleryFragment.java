0e859b1ba790a4e831ee4e9276689db22a356bf8
William <williamc825@hotmail.com>
2018-07-26 11:22:48 -0700

    forty-fifth commit, video part 45 finished.   Gallery Activity     created new snippet for top bar     setup widgets to prepare for next video

--- app/src/main/java/com/mindandmatters/william/imagegallerytesting/Share/GalleryFragment.java
+++ app/src/main/java/com/mindandmatters/william/imagegallerytesting/Share/GalleryFragment.java
@@ -2,11 +2,17 @@ package com.mindandmatters.william.imagegallerytesting.Share;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.mindandmatters.william.imagegallerytesting.R;
 
@@ -17,11 +23,42 @@ import com.mindandmatters.william.imagegallerytesting.R;
 public class GalleryFragment extends Fragment {
     private static final String TAG = "GalleryFragment";
 
+    //widgets
+    private GridView gridView;
+    private ImageView galleryImage;
+    private ProgressBar mProgressBar;
+    private Spinner directorySpinner;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_gallery, container, false);
+        galleryImage = (ImageView) view.findViewById(R.id.galleryImageView);
+        gridView = (GridView) view.findViewById(R.id.gridView);
+        directorySpinner = (Spinner) view.findViewById(R.id.spinnerDirectory);
+        mProgressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+        mProgressBar.setVisibility(View.GONE);
+
         Log.d(TAG, "onCreateView: Gallery Started");
+
+        ImageView shareClose = (ImageView) view.findViewById(R.id.ivCloseShare);
+        shareClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: closing gallery fragment");
+                getActivity().finish();
+            }
+        });
+
+        TextView nextScreen = (TextView) view.findViewById(R.id.tvNext);
+        nextScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: navigating to next screen");
+
+                //logic to open gallery, this will also be able to be accessed from EditProfile>Change profile picture
+            }
+        });
         
         return view;
     }
