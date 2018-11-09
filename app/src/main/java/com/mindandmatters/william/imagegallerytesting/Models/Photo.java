0b77eded68cf5109aa42c879ecfb6650fc5ad2d4
William <williamc825@hotmail.com>
2018-11-08 21:35:26 -0800

    seventy-fifth commit, video part 75 finished.   ViewCommentsFragment     layout_comment       created layout for comment

--- app/src/main/java/com/mindandmatters/william/imagegallerytesting/Models/Photo.java
+++ app/src/main/java/com/mindandmatters/william/imagegallerytesting/Models/Photo.java
@@ -3,6 +3,8 @@ package com.mindandmatters.william.imagegallerytesting.Models;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import org.w3c.dom.Comment;
+
 import java.lang.ref.SoftReference;
 import java.util.List;
 
@@ -19,12 +21,13 @@ public class Photo implements Parcelable{
     private String user_id;
     private String tags;
     private List<Like> likes;
+    private List<Comment> comments;
 
     public Photo() {
 
     }
 
-    public Photo(String caption, String date_created, String image_path, String photo_id, String user_id, String tags, List<Like> likes) {
+    public Photo(String caption, String date_created, String image_path, String photo_id, String user_id, String tags, List<Like> likes, List<Comment> comments) {
         this.caption = caption;
         this.date_created = date_created;
         this.image_path = image_path;
@@ -32,6 +35,7 @@ public class Photo implements Parcelable{
         this.user_id = user_id;
         this.tags = tags;
         this.likes = likes;
+        this.comments = comments;
     }
 
     protected Photo(Parcel in) {
@@ -66,6 +70,18 @@ public class Photo implements Parcelable{
                 ", tags='" + tags + '\'' +
                 ", likes=" + likes +
                 '}';
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+    public static Creator<Photo> getCREATOR() {
+        return CREATOR;
     }
 
     public String getCaption() {
--- app/src/main/java/com/mindandmatters/william/imagegallerytesting/Utils/ViewCommentsFragment.java
+++ app/src/main/java/com/mindandmatters/william/imagegallerytesting/Utils/ViewCommentsFragment.java
@@ -13,6 +13,7 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -31,11 +32,16 @@ import com.mindandmatters.william.imagegallerytesting.Models.User;
 import com.mindandmatters.william.imagegallerytesting.Models.UserAccountSettings;
 import com.mindandmatters.william.imagegallerytesting.R;
 
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.TimeZone;
 
 /**
@@ -45,6 +51,16 @@ import java.util.TimeZone;
 public class ViewCommentsFragment extends Fragment {
 
     private static final String TAG = "ViewCommentsFragment";
+
+    //FireBase auth
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference myRef;
+    private FirebaseMethods mFirebaseMethods;
+
+    //vars
+    private Photo mPhoto;
 
 
     @Nullable
@@ -57,4 +73,95 @@ public class ViewCommentsFragment extends Fragment {
         return  view;
     }
 
+
+
+    /*
+    ------------------------------------ Firebase ---------------------------------------------
+    */
+    /**
+     * Setup the firebase auth object
+     */
+    private void setupFirebaseAuth(){
+        Log.d(TAG, "setupFirebaseAuth: setting up firebase auth.");
+
+        mAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = firebaseDatabase.getReference();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+        Query query = myRef
+                .child(getString(R.string.dbname_photos))
+                .orderByChild(getString(R.string.field_photo_id))
+                .equalTo(mPhoto.getPhoto_id());
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot singleSnapshot : dataSnapshot.getChildren()){
+
+                    Photo photo = new Photo();
+                    Map<String, Object> objectMap = (HashMap<String, Object>) singleSnapshot.getValue();
+
+                    photo.setCaption(objectMap.get(getString(R.string.field_caption)).toString());
+                    photo.setTags(objectMap.get(getString(R.string.field_tags)).toString());
+                    photo.setPhoto_id(objectMap.get(getString(R.string.field_photo_id)).toString());
+                    photo.setUser_id(objectMap.get(getString(R.string.field_user_id)).toString());
+                    photo.setDate_created(objectMap.get(getString(R.string.field_date_created)).toString());
+                    photo.setImage_path(objectMap.get(getString(R.string.field_image_path)).toString());
+
+                    List<Comment> commentsList = new ArrayList<Comment>();
+                    for(DataSnapshot dSnapshot : singleSnapshot
+                            .child(getString(R.string.field_likes)).getChildren()){
+                        Comment comment = new Comment();
+                        comment.setUser_id(dataSnapshot.getValue(Like.class).getUser_id());
+                        commentList.add(comment);
+                    }
+
+//                    List<Like> likesList = new ArrayList<Like>();
+//                    for(DataSnapshot dSnapshot : singleSnapshot
+//                            .child(getString(R.string.field_likes)).getChildren()){
+//                        Like like = new Like();
+//                        like.setUser_id(dataSnapshot.getValue(Like.class).getUser_id());
+//                        likesList.add(like);
+//                    }
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d(TAG, "onCancelled: query cancelled");
+            }
+        });
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
 }
